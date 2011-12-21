5d56197826f448836ad4bab156d142bd142a6320
Annie Vang <akvang@gmail.com>
2011-12-20 22:11:51 -0600

    the end of the sample app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,31 @@
 class UsersController < ApplicationController
-  
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user,   :only => :destroy
+  before_filter :admin_user, :only => :destroy
   
+  def index
+    @users = User.paginate(:page => params[:page])
+    @title = "All users"
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
+  end
+
   def following
     @title = "Following"
     @user = User.find(params[:id])
     @users = @user.following.paginate(:page => params[:page])
     render 'show_follow'
   end
-
+  
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
     @users = @user.followers.paginate(:page => params[:page])
     render 'show_follow'
-  end
-  
-  def index
-    @title = "All users"
-    @users = User.paginate(:page => params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(:page => params[:page])
-    @title = @user.name
   end
 
   def new
@@ -38,8 +37,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
     else
       @title = "Sign up"
       render 'new'
@@ -50,25 +48,30 @@ class UsersController < ApplicationController
     @title = "Edit user"
   end
   
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_path
+  def update
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile updated." }
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
   end
-    
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
+  end
+
   private
-  
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
-    
-    def authenticate
-      deny_access unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-
+    
+    def admin_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+    end
 end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,23 +4,45 @@ describe PagesController do
   render_views
 
   before(:each) do
-    #
-    # Define @base_title here.
-    #
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    
+    describe "when not signed in" do
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+    
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      end
+
+      it "should have a non-blank body" do
+        get 'home'
+        response.body.should_not =~ /<body>\s*<\/body>/
+      end
     end
     
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                                    :content => @base_title + " | Home")
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+      
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector('a', :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector('a', :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
+
   end
 
   describe "GET 'contact'" do
@@ -32,7 +54,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                                    :content => @base_title + " | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
   end
 
@@ -45,7 +67,22 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                                    :content => @base_title + " | About")
+                                    :content => "#{@base_title} | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
     end
   end
 end
+
+
